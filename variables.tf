bc7396f0f408f6ab81b9bb0aa0ad9c4547800cb9
Dom Clayton <dominic@booandjoel.co.uk>
2023-06-27 12:17:42 +0100

    Azure Public and Private DNS zone creation

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "public_dns_zones" {
+  description = "A map of public DNS zone to create and manage"
+  type = map(object(
+    {
+      name                = string
+      resource_group_name = string
+      tags                = map(any)
+    }
+  ))
+
+}
+
+variable "private_dns_zones" {
+  description = "A map of private DNS zone to create and manage"
+  type = map(object(
+    {
+      name                = string
+      resource_group_name = string
+      soa_record = optional(object(
+        {
+          email        = string
+          expire_time  = number
+          minimum_ttl  = number
+          refresh_time = number
+          retry_time   = number
+          ttl          = number
+          tags         = map(any)
+        }),
+        {
+          email        = "azureprivatedns-host.microsoft.com"
+          expire_time  = 2419200
+          minimum_ttl  = 10
+          refresh_time = 3600
+          retry_time   = 300
+          ttl          = 3600
+          tags = {
+            defaults = "yes"
+          }
+        }
+      )
+      tags                 = map(any)
+      virtual_network_id   = string
+      virtual_network_name = string
+    }
+  ))
+
+}
